b3cfaff06b4c3fed114020896393a04c6468a705
Sezan Saimon <sezansaimon@gmail.com>
2019-04-25 16:48:01 -0400

    first commit

---
+++ Python_Practice/hello.py
@@ -0,0 +1,8 @@
+x = 1
+if x == 1:
+	print("x is 1")
+
+one = 1
+two = 2
+hello = "hello"
+	print(one + two + hello)
---
+++ practice/build/CTestTestfile.cmake
@@ -0,0 +1,7 @@
+# CMake generated Testfile for 
+# Source directory: /home/sezan/Desktop/Sezan_ws/practice/src
+# Build directory: /home/sezan/Desktop/Sezan_ws/practice/build
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
+subdirs("gtest")
